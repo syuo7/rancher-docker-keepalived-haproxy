97afb47414f5f9b37cfbeea4db033139c7facfc6
TaeilSeok <syou7@naver.com>
2017-11-28 01:38:59 +0000

    add

--- haproxy/docker-entrypoint-override.sh
+++ haproxy/docker-entrypoint-override.sh
@@ -3,7 +3,8 @@ set -e
 
 # exec haproxy entry poing script
 exec /docker-entrypoint.sh "$@" &
-
+exec rsyslogd -f /etc/haproxy/haproxy.conf &
+#exec /usr/sbin/syslogd -p=/proc/self/fd/1 &
 # start keepalived
 exec /keepalived/start-keepalived.sh
 
